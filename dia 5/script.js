bb9406d288022b56e6c6f98927687604692ee53b
cecilio <bksgki098@gmail.com>
2020-01-27 22:50:51 -0600

    dia5

---
+++ dia 5/script.js
@@ -0,0 +1,40 @@
+
+
+
+
+let nombre = prompt("Introduce tu nombre"); // String o cadena
+let edad = 30; //numero
+let ciudad = "merida";
+let datos = {Nombre:"cecilio", edad:30, ciudad:"Merida"}
+
+
+
+
+
+
+// function suma (num1, num2){
+
+//    console.log(num1 + num2);
+
+// }
+
+// suma(5, 5);
+
+
+let booleano = true; //boolean
+let booleano_falso = false; //boolean
+let coches = ["vento", "gol", "volcho"]; //Array o arreglo
+let nums = [1, 2, 3, 4, 5];
+let cosas =[1,"Hola"/ true];
+let objeto = {firstName:"John", lastName:"Perez"}; //object u objeto
+let indefinido; //undefined
+
+
+//estructuras de control
+
+if (nombre == "cecilio"){
+    console.log(" Hola cecilio!");
+} else{
+    console.log("Nombre incorrecto")
+}
+
